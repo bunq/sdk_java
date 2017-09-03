6e9fe73561c286fd88c231f898e50a6e30f4a33a
Daniil Belyakov <dnl.blkv@gmail.com>
2017-09-03 12:44:59 +0200

    remove extra import [#4]

--- src/main/java/com/bunq/sdk/json/PaginationAdapter.java
+++ src/main/java/com/bunq/sdk/json/PaginationAdapter.java
@@ -6,7 +6,6 @@ import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
-import java.io.Console;
 import java.lang.reflect.Type;
 import java.net.URI;
 import java.net.URISyntaxException;
