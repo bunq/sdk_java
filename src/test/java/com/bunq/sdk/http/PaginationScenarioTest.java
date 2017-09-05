de6fff9da021ad789c881c7611c89fdf164a1fa1
Daniil Belyakov <dnl.blkv@gmail.com>
2017-09-05 17:59:47 +0200

    Add pagiantion scenario test [#4]

--- src/test/java/com/bunq/sdk/http/PaginationScenarioTest.java
+++ src/test/java/com/bunq/sdk/http/PaginationScenarioTest.java
@@ -2,7 +2,19 @@ package com.bunq.sdk.http;
 
 import com.bunq.sdk.BunqSdkTestBase;
 import com.bunq.sdk.TestConfig;
+import com.bunq.sdk.context.ApiContext;
+import com.bunq.sdk.json.BunqGsonBuilder;
+import com.bunq.sdk.model.generated.Payment;
+import com.bunq.sdk.model.generated.object.Amount;
 import com.bunq.sdk.model.generated.object.Pointer;
+import com.google.gson.Gson;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * Tests:
@@ -16,6 +28,8 @@ public class PaginationScenarioTest extends BunqSdkTestBase {
   private static final Integer userId = TestConfig.getUserId();
   private static final Integer monetaryAccountId = TestConfig.getMonetaryAccountId();
   private static final Pointer counterPartyAliasOther = TestConfig.getCounterPartyAliasOther();
+
+  private static final ApiContext apiContext = getApiContext();
 
   /**
    * Constants for scenario testing.
@@ -31,4 +45,70 @@ public class PaginationScenarioTest extends BunqSdkTestBase {
   private final static String FIELD_CURRENCY = "EUR";
   private final static String FIELD_PAYMENT_DESCRIPTION = "C# test Payment";
 
+  /**
+   * Gson builder for serialization.
+   */
+  private static Gson gson = BunqGsonBuilder.buildDefault().create();
+
+  @Test
+  public void apiScenarioPaymentListingWithPaginationTest()
+  {
+    EnsureEnoughPayments();
+    Set paymentsExpected = new LinkedHashSet<>(GetPaymentsRequired());
+    Pagination paginationCountOnly = new Pagination();
+    paginationCountOnly.setCount(PAYMENT_LISTING_PAGE_SIZE);
+
+    BunqResponse<List<Payment>> paymentResponseLatest =
+        ListPayments(paginationCountOnly.getUrlParamsCountOnly());
+    Pagination paginationLatest = paymentResponseLatest.getPagination();
+    BunqResponse<List<Payment>> paymentResponsePrevious =
+        ListPayments(paginationLatest.getUrlParamsPreviousPage());
+    Pagination paginationPrevious = paymentResponsePrevious.getPagination();
+    BunqResponse<List<Payment>> paymentResponsePreviousNext =
+        ListPayments(paginationPrevious.getUrlParamsNextPage());
+
+    Set<Payment> paymentsActual = new LinkedHashSet<>();
+    paymentsActual.addAll(paymentResponsePreviousNext.getValue());
+    paymentsActual.addAll(paymentResponsePrevious.getValue());
+    String paymentsExpectedSerialized = gson.toJson(paymentsExpected);
+    String paymentsActualSerialized = gson.toJson(paymentsActual);
+
+    Assert.assertEquals(paymentsExpectedSerialized, paymentsActualSerialized);
+  }
+
+  private static void EnsureEnoughPayments()
+  {
+    for (int i = NUMBER_ZERO; i < GetPaymentsMissingCount(); ++i)
+    {
+      CreatePayment();
+    }
+  }
+
+  private static int GetPaymentsMissingCount()
+  {
+    return PAYMENT_REQUIRED_COUNT_MINIMUM - GetPaymentsRequired().size();
+  }
+
+  private static List<Payment> GetPaymentsRequired()
+  {
+    Pagination pagination = new Pagination();
+    pagination.setCount(PAYMENT_REQUIRED_COUNT_MINIMUM);
+
+    return ListPayments(pagination.getUrlParamsCountOnly()).getValue();
+  }
+
+  private static BunqResponse<List<Payment>> ListPayments(Map<String, String> urlParams)
+  {
+    return Payment.list(apiContext, userId, monetaryAccountId, urlParams);
+  }
+
+  private static void CreatePayment()
+  {
+    Map<String, Object> requestMap = new HashMap<>();
+    requestMap.put(Payment.FIELD_AMOUNT, new Amount(AMOUNT_IN_EUR, FIELD_CURRENCY));
+    requestMap.put(Payment.FIELD_DESCRIPTION, FIELD_PAYMENT_DESCRIPTION);
+    requestMap.put(Payment.FIELD_COUNTERPARTY_ALIAS, counterPartyAliasOther);
+
+    Payment.create(apiContext, requestMap, userId, monetaryAccountId);
+  }
 }
