86e1e2efe5f7218fed270e9dd1f745e151f7eb6e
Kevin Hellemun <khellemun@bunq.com>
2017-12-30 09:33:21 +0100

    Change exception base class to hold response id. (bunq/sdk_java#55)

--- src/main/java/com/bunq/sdk/exception/ApiException.java
+++ src/main/java/com/bunq/sdk/exception/ApiException.java
@@ -3,15 +3,21 @@ package com.bunq.sdk.exception;
 public class ApiException extends RuntimeException {
 
   private int responseCode;
+  private String responseId;
 
-  public ApiException(String message, int responseCode) {
+  public ApiException(String message, int responseCode, String responseId) {
     super(message);
 
     this.responseCode = responseCode;
+    this.responseId = responseId;
   }
 
   public int getResponseCode() {
     return responseCode;
+  }
+
+  public String getResponseId() {
+    return responseId;
   }
 
 }
