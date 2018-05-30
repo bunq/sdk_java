16f5ff94d70e04caaaac70f584efd9c337ccb1a9
Kevin Hellemun <khellemun@bunq.com>
2018-05-30 11:32:05 +0200

    Version bump to 0.13.1

--- src/main/java/com/bunq/sdk/http/ApiClient.java
+++ src/main/java/com/bunq/sdk/http/ApiClient.java
@@ -90,7 +90,7 @@ public class ApiClient {
   /**
    * Prefix for bunq's own headers.
    */
-  private static final String USER_AGENT_BUNQ = "bunq-sdk-java/0.13.0";
+  private static final String USER_AGENT_BUNQ = "bunq-sdk-java/0.13.1";
   private static final String LANGUAGE_EN_US = "en_US";
   private static final String REGION_NL_NL = "nl_NL";
   private static final String GEOLOCATION_ZERO = "0 0 0 0 000";
