553c4f10ff9a9eae9566f5ed1b8af9f0695e75ec
bunq <github@bunq.com>
2021-11-26 12:08:53 +0000

    Regenerated code based on the latest tag

--- src/main/java/com/bunq/sdk/model/generated/object/AttachmentTab.java
+++ src/main/java/com/bunq/sdk/model/generated/object/AttachmentTab.java
@@ -1,93 +1,96 @@
 package com.bunq.sdk.model.generated.object;
 
 import com.bunq.sdk.model.core.BunqModel;
+import com.bunq.sdk.model.core.MonetaryAccountReference;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 import com.google.gson.stream.JsonReader;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
- *
  */
 public class AttachmentTab extends BunqModel {
 
-    /**
-     * The id of the attachment.
-     */
-    @Expose
-    @SerializedName("id")
-    private Integer id;
+  /**
+   * The id of the attachment.
+   */
+  @Expose
+  @SerializedName("id")
+  private Integer id;
 
-    /**
-     * The description of the attachment.
-     */
-    @Expose
-    @SerializedName("description")
-    private String description;
+  /**
+   * The description of the attachment.
+   */
+  @Expose
+  @SerializedName("description")
+  private String description;
 
-    /**
-     * The content type of the attachment's file.
-     */
-    @Expose
-    @SerializedName("content_type")
-    private String contentType;
+  /**
+   * The content type of the attachment's file.
+   */
+  @Expose
+  @SerializedName("content_type")
+  private String contentType;
 
-    /**
-     *
-     */
-    public static AttachmentTab fromJsonReader(JsonReader reader) {
-        return fromJsonReader(AttachmentTab.class, reader);
+  /**
+   * The id of the attachment.
+   */
+  public Integer getId() {
+    return this.id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  /**
+   * The description of the attachment.
+   */
+  public String getDescription() {
+    return this.description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  /**
+   * The content type of the attachment's file.
+   */
+  public String getContentType() {
+    return this.contentType;
+  }
+
+  public void setContentType(String contentType) {
+    this.contentType = contentType;
+  }
+
+  /**
+   */
+  public boolean isAllFieldNull() {
+    if (this.id != null) {
+      return false;
     }
 
-    /**
-     * The id of the attachment.
-     */
-    public Integer getId() {
-        return this.id;
+    if (this.description != null) {
+      return false;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    if (this.contentType != null) {
+      return false;
     }
 
-    /**
-     * The description of the attachment.
-     */
-    public String getDescription() {
-        return this.description;
-    }
+    return true;
+  }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    /**
-     * The content type of the attachment's file.
-     */
-    public String getContentType() {
-        return this.contentType;
-    }
-
-    public void setContentType(String contentType) {
-        this.contentType = contentType;
-    }
-
-    /**
-     *
-     */
-    public boolean isAllFieldNull() {
-        if (this.id != null) {
-            return false;
-        }
-
-        if (this.description != null) {
-            return false;
-        }
-
-        if (this.contentType != null) {
-            return false;
-        }
-
-        return true;
-    }
+  /**
+   */
+  public static AttachmentTab fromJsonReader(JsonReader reader) {
+    return fromJsonReader(AttachmentTab.class, reader);
+  }
 
 }
