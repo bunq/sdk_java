1a96125a3e1408e22c30d479aadd2d0b94f10776
bunq <github@bunq.com>
2024-11-28 20:36:06 +0000

    Regenerated code based on the latest tag

--- src/main/java/com/bunq/sdk/model/generated/endpoint/CompanyEmployeeCard.java
+++ src/main/java/com/bunq/sdk/model/generated/endpoint/CompanyEmployeeCard.java
@@ -20,7 +20,8 @@ public class CompanyEmployeeCard extends BunqModel {
   /**
    * Field constants.
    */
-  public static final String FIELD_ALIAS = "alias";
+  public static final String FIELD_POINTER_COUNTER_USER = "pointer_counter_user";
+  public static final String FIELD_POINTER_MONETARY_ACCOUNT = "pointer_monetary_account";
   public static final String FIELD_TYPE = "type";
   public static final String FIELD_PRODUCT_TYPE = "product_type";
   public static final String FIELD_COMPANY_NAME_ON_CARD = "company_name_on_card";
@@ -35,13 +36,6 @@ public class CompanyEmployeeCard extends BunqModel {
   @Expose
   @SerializedName("card")
   private Card card;
-
-  /**
-   * The id of the relation user.
-   */
-  @Expose
-  @SerializedName("relation_user_id")
-  private Integer relationUserId;
 
   /**
    * The status of the employee card.
@@ -72,11 +66,25 @@ public class CompanyEmployeeCard extends BunqModel {
   private Amount amountSpentMonthly;
 
   /**
+   * The number of transactions that still need a receipt.
+   */
+  @Expose
+  @SerializedName("number_of_company_employee_card_receipt_pending")
+  private Integer numberOfCompanyEmployeeCardReceiptPending;
+
+  /**
+   * The pointer to the employee for which you want to create a card.
+   */
+  @Expose
+  @SerializedName("pointer_counter_user_field_for_request")
+  private Pointer pointerCounterUserFieldForRequest;
+
+  /**
    * The pointer to the monetary account that will be connected at first with the card.
    */
   @Expose
-  @SerializedName("alias_field_for_request")
-  private Pointer aliasFieldForRequest;
+  @SerializedName("pointer_monetary_account_field_for_request")
+  private Pointer pointerMonetaryAccountFieldForRequest;
 
   /**
    * The type of card to order.
@@ -128,39 +136,44 @@ public class CompanyEmployeeCard extends BunqModel {
   private String statusFieldForRequest;
 
   public CompanyEmployeeCard() {
-  this(null, null, null, null, null, null, null, null);
+  this(null, null, null, null, null, null, null, null, null);
   }
 
-  public CompanyEmployeeCard(Pointer alias) {
-  this(alias, null, null, null, null, null, null, null);
+  public CompanyEmployeeCard(Pointer pointerCounterUser) {
+  this(pointerCounterUser, null, null, null, null, null, null, null, null);
   }
 
-  public CompanyEmployeeCard(Pointer alias, String type) {
-  this(alias, type, null, null, null, null, null, null);
+  public CompanyEmployeeCard(Pointer pointerCounterUser, Pointer pointerMonetaryAccount) {
+  this(pointerCounterUser, pointerMonetaryAccount, null, null, null, null, null, null, null);
   }
 
-  public CompanyEmployeeCard(Pointer alias, String type, String productType) {
-  this(alias, type, productType, null, null, null, null, null);
+  public CompanyEmployeeCard(Pointer pointerCounterUser, Pointer pointerMonetaryAccount, String type) {
+  this(pointerCounterUser, pointerMonetaryAccount, type, null, null, null, null, null, null);
   }
 
-  public CompanyEmployeeCard(Pointer alias, String type, String productType, String companyNameOnCard) {
-  this(alias, type, productType, companyNameOnCard, null, null, null, null);
+  public CompanyEmployeeCard(Pointer pointerCounterUser, Pointer pointerMonetaryAccount, String type, String productType) {
+  this(pointerCounterUser, pointerMonetaryAccount, type, productType, null, null, null, null, null);
   }
 
-  public CompanyEmployeeCard(Pointer alias, String type, String productType, String companyNameOnCard, String employeeNameOnCard) {
-  this(alias, type, productType, companyNameOnCard, employeeNameOnCard, null, null, null);
+  public CompanyEmployeeCard(Pointer pointerCounterUser, Pointer pointerMonetaryAccount, String type, String productType, String companyNameOnCard) {
+  this(pointerCounterUser, pointerMonetaryAccount, type, productType, companyNameOnCard, null, null, null, null);
   }
 
-  public CompanyEmployeeCard(Pointer alias, String type, String productType, String companyNameOnCard, String employeeNameOnCard, String employeePreferredNameOnCard) {
-  this(alias, type, productType, companyNameOnCard, employeeNameOnCard, employeePreferredNameOnCard, null, null);
+  public CompanyEmployeeCard(Pointer pointerCounterUser, Pointer pointerMonetaryAccount, String type, String productType, String companyNameOnCard, String employeeNameOnCard) {
+  this(pointerCounterUser, pointerMonetaryAccount, type, productType, companyNameOnCard, employeeNameOnCard, null, null, null);
   }
 
-  public CompanyEmployeeCard(Pointer alias, String type, String productType, String companyNameOnCard, String employeeNameOnCard, String employeePreferredNameOnCard, Amount amountLimitMonthly) {
-  this(alias, type, productType, companyNameOnCard, employeeNameOnCard, employeePreferredNameOnCard, amountLimitMonthly, null);
+  public CompanyEmployeeCard(Pointer pointerCounterUser, Pointer pointerMonetaryAccount, String type, String productType, String companyNameOnCard, String employeeNameOnCard, String employeePreferredNameOnCard) {
+  this(pointerCounterUser, pointerMonetaryAccount, type, productType, companyNameOnCard, employeeNameOnCard, employeePreferredNameOnCard, null, null);
   }
 
-  public CompanyEmployeeCard(Pointer alias, String type, String productType, String companyNameOnCard, String employeeNameOnCard, String employeePreferredNameOnCard, Amount amountLimitMonthly, String status) {
-    this.aliasFieldForRequest = alias;
+  public CompanyEmployeeCard(Pointer pointerCounterUser, Pointer pointerMonetaryAccount, String type, String productType, String companyNameOnCard, String employeeNameOnCard, String employeePreferredNameOnCard, Amount amountLimitMonthly) {
+  this(pointerCounterUser, pointerMonetaryAccount, type, productType, companyNameOnCard, employeeNameOnCard, employeePreferredNameOnCard, amountLimitMonthly, null);
+  }
+
+  public CompanyEmployeeCard(Pointer pointerCounterUser, Pointer pointerMonetaryAccount, String type, String productType, String companyNameOnCard, String employeeNameOnCard, String employeePreferredNameOnCard, Amount amountLimitMonthly, String status) {
+    this.pointerCounterUserFieldForRequest = pointerCounterUser;
+    this.pointerMonetaryAccountFieldForRequest = pointerMonetaryAccount;
     this.typeFieldForRequest = type;
     this.productTypeFieldForRequest = productType;
     this.companyNameOnCardFieldForRequest = companyNameOnCard;
@@ -179,17 +192,6 @@ public class CompanyEmployeeCard extends BunqModel {
 
   public void setCard(Card card) {
     this.card = card;
-  }
-
-  /**
-   * The id of the relation user.
-   */
-  public Integer getRelationUserId() {
-    return this.relationUserId;
-  }
-
-  public void setRelationUserId(Integer relationUserId) {
-    this.relationUserId = relationUserId;
   }
 
   /**
@@ -237,13 +239,20 @@ public class CompanyEmployeeCard extends BunqModel {
   }
 
   /**
+   * The number of transactions that still need a receipt.
+   */
+  public Integer getNumberOfCompanyEmployeeCardReceiptPending() {
+    return this.numberOfCompanyEmployeeCardReceiptPending;
+  }
+
+  public void setNumberOfCompanyEmployeeCardReceiptPending(Integer numberOfCompanyEmployeeCardReceiptPending) {
+    this.numberOfCompanyEmployeeCardReceiptPending = numberOfCompanyEmployeeCardReceiptPending;
+  }
+
+  /**
    */
   public boolean isAllFieldNull() {
     if (this.card != null) {
-      return false;
-    }
-
-    if (this.relationUserId != null) {
       return false;
     }
 
@@ -260,6 +269,10 @@ public class CompanyEmployeeCard extends BunqModel {
     }
 
     if (this.amountSpentMonthly != null) {
+      return false;
+    }
+
+    if (this.numberOfCompanyEmployeeCardReceiptPending != null) {
       return false;
     }
 
